50f5f0737de4f09c56fa318f7f9391da4a57ba1f
Tharushi99 <tharushijayakody1999@gmail.com>
2020-09-26 01:00:16 +0530

    add firebase

--- app/src/main/java/com/example/android_app_food_g6/ActivityDelivery/RegisterDelivery.java
+++ app/src/main/java/com/example/android_app_food_g6/ActivityDelivery/RegisterDelivery.java
@@ -1,12 +1,23 @@
 package com.example.android_app_food_g6.ActivityDelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatMultiAutoCompleteTextView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import com.example.android_app_g6.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class RegisterDelivery extends AppCompatActivity {
+    AppCompatMultiAutoCompleteTextView fname, lname, uname, email, password, confirmpass;
+    AppCompatButton registerbtn;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
