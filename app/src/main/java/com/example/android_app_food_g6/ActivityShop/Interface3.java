2ee0f4a3490db0df4339488209644461fa2d0726
Heshanidassanayake <heshanidassanayake03@gmail.com>
2020-09-26 21:28:10 +0530

    add new activitys

---
+++ app/src/main/java/com/example/android_app_food_g6/ActivityShop/Interface3.java
@@ -0,0 +1,16 @@
+package com.example.android_app_food_g6.ActivityShop;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.android_app_g6.R;
+
+public class Interface3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interface3_shop);
+    }
+}
